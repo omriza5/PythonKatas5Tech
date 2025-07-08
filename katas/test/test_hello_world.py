14f3b640c0a7c6d7144cbec42db8a1c30b82c152
omri zaher <omriza5@gmail.com>
2025-07-08 10:15:39 +0300

    Add .gitignore and enhance tests for hello_world function

--- katas/test/test_hello_world.py
+++ katas/test/test_hello_world.py
@@ -5,3 +5,6 @@ from katas.hello_world import hello_world
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "hello world")
+    def test_hello_world_return_type(self):
+        """Test that the function returns a string."""
+        self.assertIsInstance(hello_world(), str)
